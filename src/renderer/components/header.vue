625c37d8ec017315a1cb79f7546a59164e72bd87
Suyashtnt <suyashtnt@gmail.com>
2021-01-07 18:48:22 +0200

    feat(Header): navigation

--- src/renderer/components/header.vue
+++ src/renderer/components/header.vue
@@ -4,19 +4,52 @@
       <v-app-bar-nav-icon @click="sidebarVisible = !sidebarVisible" />
       <v-toolbar-title> Glowsquid </v-toolbar-title>
     </v-toolbar>
-    <v-navigation-drawer v-model="sidebarVisible" absolute left temporary>
-      test
+    <v-navigation-drawer v-model="sidebarVisible" absolute temporary>
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          @click="$router.push({
+            path: item.path
+          })"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block @click="$router.push({
+            path: '/settings'
+          })"
+          >
+            Settings
+          </v-btn>
+        </div>
+      </template>
     </v-navigation-drawer>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   data () {
     return {
-      sidebarVisible: false
+      sidebarVisible: false,
+      items: [{
+        title: 'Home',
+        icon: 'mdi-home',
+        path: '/'
+      }] as {title: string, icon: string, path: string}[]
     }
   }
 })
