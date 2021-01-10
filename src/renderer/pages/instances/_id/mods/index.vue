cf7c17aa6ff098b208226b9a2612bbca75c25b97
Suyashtnt <suyashtnt@gmail.com>
2021-01-10 21:35:09 +0200

    feat(mod-card): expand actions to full width well... 98% of the width

--- src/renderer/pages/instances/_id/mods/index.vue
+++ src/renderer/pages/instances/_id/mods/index.vue
@@ -4,15 +4,20 @@
     class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2"
   >
     <v-card v-for="mod in modList.hits" :key="mod.mod_id" tile height="100%" class="card-outter">
-      <v-card-title class="flex flex-row">
+      <v-card-title class="flex flex-row mb-2">
         <v-img :max-height="32" :max-width="32" :src="mod.icon_url" class="mr-3" />
         <p class="h-full ma-0 align-middle">{{ mod.title }}</p>
       </v-card-title>
-      <v-card-subtitle> {{ mod.author }}</v-card-subtitle>
+      <v-card-subtitle>by {{ mod.author }}</v-card-subtitle>
       <v-card-text> {{ mod.description }}</v-card-text>
-      <v-card-actions class="card-actions">
-        <v-btn>about</v-btn>
-        <v-btn>install</v-btn>
+
+      <!-- We need 98% width because else the install button will hit the side for some reason -->
+      <v-card-actions
+        class="card-actions grid-cols-2 gap-1 justify-center"
+        style="display: grid !important; width: 98%;"
+      >
+        <v-btn block>about</v-btn>
+        <v-btn block>install</v-btn>
       </v-card-actions>
     </v-card>
   </div>
