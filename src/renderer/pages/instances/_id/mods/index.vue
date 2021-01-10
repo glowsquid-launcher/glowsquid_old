592fcb541f608d90b80c8beb884c3a9bcf66d54a
Suyashtnt <suyashtnt@gmail.com>
2021-01-10 20:59:22 +0200

    feat(mod-card): make cards responsive and add img

--- src/renderer/pages/instances/_id/mods/index.vue
+++ src/renderer/pages/instances/_id/mods/index.vue
@@ -1,10 +1,16 @@
 <template>
-  <div v-if="modList.hits" class="grid grid-cols-3 gap-2">
-    <v-card v-for="mod in modList.hits" :key="mod.mod_id" tile>
-      <v-card-title> {{ mod.title }}</v-card-title>
+  <div
+    v-if="modList.hits"
+    class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2"
+  >
+    <v-card v-for="mod in modList.hits" :key="mod.mod_id" tile height="100%" class="card-outter">
+      <v-card-title class="flex flex-row">
+        <v-img :max-height="32" :max-width="32" :src="mod.icon_url" class="mr-3" />
+        <p class="h-full ma-0 align-middle">{{ mod.title }}</p>
+      </v-card-title>
       <v-card-subtitle> {{ mod.author }}</v-card-subtitle>
       <v-card-text> {{ mod.description }}</v-card-text>
-      <v-card-actions>
+      <v-card-actions class="card-actions">
         <v-btn>about</v-btn>
         <v-btn>install</v-btn>
       </v-card-actions>
@@ -29,3 +35,14 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="stylus">
+.card-outter {
+  position: relative
+  padding-bottom: 50px
+}
+.card-actions {
+  position: absolute
+  bottom: 0
+}
+</style>
