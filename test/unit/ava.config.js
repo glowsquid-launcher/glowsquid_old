5eae8c8d94d2fed00ee579c1d928e8bdc88c4bd2
Suyashtnt <suyashtnt@gmail.com>
2021-01-25 12:15:57 +0200

    feat: try to get unit tests working but im too dumb

--- test/unit/ava.config.js
+++ test/unit/ava.config.js
@@ -11,7 +11,12 @@ export default {
     'src/**/*'
   ],
   require: [
-    './test/unit/setup.js'
+    './test/unit/setup.js',
+    'ts-node/register'
+  ],
+  compileEnhancements: false,
+  extensions: [
+    'ts'
   ],
   babel: {
     testOptions: {
